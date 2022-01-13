532d49fc2a1231d791fca1e226ef744eabe3c06b
Shahzodbek <94956469+Shohzod-xv@users.noreply.github.com>
2022-01-13 10:59:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -23,26 +23,25 @@
     $tx = $message->text;
     date_default_timezone_set('Asia/Tashkent');
     $hozir = date(" G:i ");    
-// ============================================
+
+
+
 $city_name = $tx;
-$pogoda_api = 'OpenWeatherApiTOKEN';
+$pogoda_api = 'OpenWeather_TOKEN';
 $pogoda_url = 'http://api.openweathermap.org/data/2.5/weather?q='.$city_name.'&appid='.$pogoda_api;
 $pogoda_data = json_decode(file_get_contents($pogoda_url), true);
 
-$temp = round($pogoda_data['main']['temp'] - 273.15)." °C";
+$temp = round($pogoda_data['main']['temp'] - 273.15)." Â°C";
 $havo = $pogoda_data['weather'][0]['main'];
 $nam = $pogoda_data['main']['humidity']." %";
-$wind = round($pogoda_data['wind']['speed'])." m/s tezlikka yetishi mumkin";
+$wind = round($pogoda_data['wind']['speed']);
 
-// //Checking
-if ($temp == "-273 °C") {
+ //Checking
+if ($temp == "-273 Â°C") {
     $temp = 'topilmadi';
 }
 if ($nam == " %") {
     $nam = "topilmadi";
-}
-if ($wind = "0 m/s tezlikka yetishi mumkin") {
-    $wind = "Shamol yo'q";
 }
 // Start
 if($tx == "/start"){
@@ -60,13 +59,13 @@ if($tx == "/start"){
             }
         elseif ($havo == "Clear") 
             {
-                $havo = "Ochiq Havo";
+                $havo = "Ochiq";
                 $havo_photo = '[.](https://telegra.ph/file/a5025719a2045e3fcc388.jpg)';
             }
         elseif ($havo == "Rain") 
             {
-                $havo = "Yomg'irli havo";
-                $havo_photo = '[.](https://telegra.ph/file/285eb409dd3679b07612e.jpg)';
+                $havo = "Yomg'ir";
+                $havo_photo = '[.](https://telegra.ph/file/168e5d9642a38e4673676.jpg)';
             }
         elseif ($havo == "Snow") 
             {
@@ -75,7 +74,7 @@ if($tx == "/start"){
             }
         elseif (($havo == "Haze") || ($havo == "Mist") || ($havo == "Smoke") || ($havo == "Fog") || ($havo == "Cape") || ($havo == "Brume")) 
             {
-                $havo = "Tumanli havo";
+                $havo = "Tuman";
                 $havo_photo = '[.](https://telegra.ph/file/f9d606f414f910c1b097f.jpg)';
             }
         elseif($havo == "")
@@ -83,13 +82,19 @@ if($tx == "/start"){
             $havo = "topilmadi";
             $havo_photo = '[.](https://telegra.ph/file/38b47b11724a1ed6b7b6f.png)';
         }
+
+        if ($wind == " m/s tezlikka yetishi mumkin") {
+            $wind = "Yo'q";
+        }else {
+            $wind = $wind." m/s tezlikka yetishi mumkin";
+        }
             bot('sendMessage', [
                 'chat_id' => $cid,
                 'text' => "*$city_name - $hozir*\n
 *Harorat:  *$temp
 *Havo - *$havo
 *Namlik - *$nam
-*Shamol - *$wind $havo_photo",
+*Shamol - *$wind  $havo_photo",
                     'parse_mode' => 'markdown',
                 ]);
     }
